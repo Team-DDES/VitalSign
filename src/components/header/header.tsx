25c92a943f6530d1f7f7e932f4138446035b5aec
DESKTOP-8QPPU7Q\Jupiter <pental_vega@naver.com>
2023-06-03 20:16:15 +0900

    Add Innopia Logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,19 +3,25 @@ import styles from './header.module.scss';
 
 const Header = () => (
   <header className={styles.header}>
-    <view style={{padding: 2, marginTop: 5, marginBottom: 5}}>
-      <a href="https://github.com/ubicomplab/rppg-web" target="_blank">
+    <div style={{ display: 'flex', justifyContent: 'space-between', padding: 2, marginTop: 5, marginBottom: 5 }}>
+      <a href="https://worldcoin.org/" target="_blank" rel="noopener noreferrer">
         <img
           src="images/worldcoin_logo.png"
           width="188"
           height="32"
         />
       </a>
-      <br></br>
-      <div style={{fontSize: '20px'}}>
-      THE GLOBAL ECONOMY BELONGS TO EVERYONE.
-      </div> 
-    </view>
+      <div style={{fontSize: '20px', fontWeight: 'bold', alignSelf: 'center' }}>
+        rPPG with WORLDCOIN
+      </div>
+      <a href="https://github.com/Team-DDES/VitalSign" target="_blank" rel="noopener noreferrer">
+        <img
+          src="images/innopia_logo.png"
+          width="188"
+          height="32"
+        />
+      </a>
+    </div>
   
   </header>
 );
